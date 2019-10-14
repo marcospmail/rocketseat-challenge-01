e4840ebdeec88bcf90315eb27b8e8bbc434cc77e
Marcos Paulo <marcosp.mail@gmail.com>
2019-10-14 17:21:16 -0300

    finishes challenge 01

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 const express = require('express')
-
 const server = express()
 
 server.use(express.json())
+
+var requestsCount = 0
+const data = [];
+
+function isValidId(req, res, next) {
+    const project = data.find(project => project.id === req.params.id)
+    if (!project) {
+        return res.status(400).json({message: 'invalid id'})
+    }
+
+    return next()
+}
+
+server.use((req, res, next) => {
+    requestsCount ++;
+    console.log(requestsCount)
+
+    next();
+})
+
+server.post('/projects', (req, res) => {
+    const { id, title } = req.body;
+
+    const project = {
+        id,
+        title,
+        tasks: []
+    }
+
+    data.push(project)
+
+    res.json(project)
+})
+
+server.put('/projects/:id', isValidId, (req, res) => {
+    const { id } = req.params
+    const project = data.find(project => project.id === id)
+
+    project.title = req.body.title
+
+    res.json(project)
+})
+
+server.post('/projects/:id/tasks', isValidId, (req, res) => {
+    const { id } = req.params;
+    
+    data[id].tasks.push(req.body.title)
+
+    res.json(data[id].tasks)
+})
+
+server.get('/projects', (req, res) => {
+    res.json(data)
+})
 
 server.listen(3000)
